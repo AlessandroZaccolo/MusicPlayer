0524ce6658fa8fc14d2bcbfaf5f8440db8aa8d5e
alessandro.zaccolo <alessandro.zaccolo@bts.tech>
2019-07-01 06:10:20 +0200

    Service to play music in the background

--- app/src/main/java/com/example/musicplayer/service/MusicPlayerService.java
+++ app/src/main/java/com/example/musicplayer/service/MusicPlayerService.java
@@ -1,24 +1,65 @@
 package com.example.musicplayer.service;
 
-import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.IBinder;
-import android.provider.MediaStore;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+
 import java.io.IOException;
 
-public class MusicPlayerService extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
-MediaPlayer.OnInfoListener{
+public class MusicPlayerService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener{
 
-    private MediaPlayer mediaPlayerInService;
-    private String data;
+    private MediaPlayer player = null;
 
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+
+        String data = (String) intent.getExtras().get("audio");
+        String action = (String) intent.getExtras().get("action");
+
+
+        if(player == null && action.equals("PLAY")){
+            createAndConfigMediaPlayer(data);
+
+            player.prepareAsync();
+        }
+
+        if(player.isPlaying() && action.equals("PAUSE")){
+            player.pause();
+        } else if(!player.isPlaying() && action.equals("PAUSE")){
+            player.start();
+        }
+
+        if(player.isPlaying() && action.equals("PLAY")){
+            player.stop();
+            createAndConfigMediaPlayer(data);
+            player.prepareAsync();
+        }
+
+
+
+        return START_STICKY;
+    }
+
+
+    private void createAndConfigMediaPlayer(String data) {
+        player = new MediaPlayer();
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        final Uri mediaPath = Uri.parse("android.resource://" + getPackageName() + "/raw/" + data);
+
+        try {
+            player.setDataSource(getApplicationContext(), mediaPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Nullable
     @Override
@@ -28,69 +69,12 @@ MediaPlayer.OnInfoListener{
 
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-
-        Toast.makeText(this, "Service starts", Toast.LENGTH_SHORT).show();
-
-        data = (String) intent.getExtras().get("audio");
-
-
-        if (mediaPlayerInService == null) {
-
-            Toast.makeText(this, "Playing song "+ data, Toast.LENGTH_LONG).show();
-
-
-            createAndConfigMediaPlayer(data);
-        }
-        if (mediaPlayerInService.isPlaying()) {
-
-            Toast.makeText(this, "Song is paused", Toast.LENGTH_LONG).show();
-
-
-            mediaPlayerInService.stop();
-        }
-        mediaPlayerInService.prepareAsync();
-
-
-        return START_STICKY;
-    }
-
-
-
-    private void createAndConfigMediaPlayer(String data) {
-        mediaPlayerInService = new MediaPlayer();
-        mediaPlayerInService.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        mediaPlayerInService.setOnPreparedListener(this);
-        mediaPlayerInService.setOnCompletionListener(this);
-        final Uri mediaPath = Uri.parse("android.resource://" + getPackageName() + "/raw/" + data);
-
-        Toast.makeText(this, "it is playing "+ mediaPath, Toast.LENGTH_LONG).show();
-        try {
-            mediaPlayerInService.setDataSource(getApplicationContext(), mediaPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        mediaPlayerInService.stop();
-    }
-
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-        mp.release();
-    }
-
-    @Override
     public void onPrepared(MediaPlayer mp) {
         mp.start();
     }
 
     @Override
-    public boolean onInfo(MediaPlayer mp, int what, int extra) {
-        return false;
+    public void onCompletion(MediaPlayer mp) {
+        mp.release();
     }
 }
